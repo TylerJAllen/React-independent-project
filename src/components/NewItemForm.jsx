08d1adb083265fb7e75c8feda6baac8c6b2a78a0
Tyler Allen <mynameistylerallen@gmail.com>
2017-08-25 14:05:43 -0700

    add form to NewItemForm.jsx using react-bootstrap components.

--- src/components/NewItemForm.jsx
+++ src/components/NewItemForm.jsx
@@ -1,17 +1,39 @@
 import React from 'react';
+import item from '../models/item.js';
 import PropTypes from 'prop-types';
+import {Form, InputGroup, FormGroup, ControlLabel, FormControl} from 'react-bootstrap';
 // import styles from '../styles/App.css';
 
 
-function NewItemForm(props){
-  return (
-    <div>
-      <h1>New Item Form</h1>
-    </div>
-  );
+class NewItemForm extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <div>
+        <form>
+          <FormGroup controlId="formControlsText">
+            <ControlLabel>What would you like me to keep track of?</ControlLabel>
+            <FormControl
+              id="formControlsText"
+              type="text"
+              />
+          </FormGroup>
+          <FormGroup controlId="formControlsTextarea">
+            <ControlLabel>Add notes that will help me help you. <em>Update this section with specifics such as favorites, dates, locations, previous events, etc.</em></ControlLabel>
+            <FormControl componentClass="textarea" placeholder="Notes" />
+          </FormGroup>
+        </form>
+      </div>
+    );
+  }
 }
 
 NewItemForm.propTypes = {
+  onNewItemCreation: PropTypes.func,
+  hideFormAfterSubmission: PropTypes.func
 };
 
 export default NewItemForm;
