222deca7a22ce62daea2db6a1e2841c8122aeda3
Tyler Allen <mynameistylerallen@gmail.com>
2017-08-25 12:37:21 -0700

    add modal to NewItemControl.jsx.

--- src/components/NewItemControl.jsx
+++ src/components/NewItemControl.jsx
@@ -2,19 +2,32 @@ import React from 'react';
 import NewItemForm from './NewItemForm';
 import PropTypes from 'prop-types';
 import { Button }  from "react-bootstrap";
+import { Modal } from "react-bootstrap";
 // import styles from '../styles/App.css';
 
 
 function NewItemControl(props){
   return (
     <div>
-      <button
+      <Button
         block
-        bsStyle=""
+        bsStyle="info"
         bsSize="large">
         Add Item
-      </button>
-      <NewItemForm />
+      </Button>
+      <Modal
+        show={this.state.formModalIsShowing}
+        onHide={this.hideFormModal}
+        bsSize="large">
+        <Modal.Header>
+          <Modal.Title>Modal Title</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <NewItemForm
+            onNewItemCreation={this.props.onNewItemCreation}
+            hideFormAfterSubmission = {this.hideFormModal}/>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 }
