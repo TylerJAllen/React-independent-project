3bc30461f136b76ab928a3bffaea72a3919a6b95
Tyler Allen <mynameistylerallen@gmail.com>
2017-08-25 08:15:13 -0700

    create boilerplate

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../styles/App.css';
+
+function App(){
+  return (
+    <div className={styles.app}>
+      <h1>Friday Project</h1>
+    </div>
+  );
+}
+
+export default App;
